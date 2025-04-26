2f2bbfa3d75155246ee167e4c0a2c98ea8b27311
Greta Imhof <gni97102@uga.edu>
2025-04-25 20:23:22 -0400

    server

--- cinema-e-booking-system/backend/server.js
+++ cinema-e-booking-system/backend/server.js
@@ -18,7 +18,7 @@ const connectDB = async () => {
         const db = await mysql.createConnection({
             host: "127.0.0.1",
             user: "root", // Update if your DB user is different
-            password: "urmomspicklejar", // Update with your DB password
+            password: "", // Update with your DB password
             database: "cinema_ebooking"
         });
         console.log("Connected to database");
